ce1fd6ccabbd3ad0529ef58e66858d34ec24669c
NotMaxivelow01 <mfont@endenschool.fr>
2022-09-23 15:08:40 +0200

    code

--- js/app.js
+++ js/app.js
@@ -7,3 +7,29 @@ age = 15;
 // prenom est un string //
 const prenom = "Maxime" ;
 
+let addition = 4+12;
+console.log(addition);
+
+let soustraction = 2-50;
+console.log(soustraction);
+
+let multiplication = 3*8; 
+console.log(multiplication);
+
+let division = 7/2; 
+console.log(division)
+
+let modulo = 5%2;
+console.log(modulo)
+
+//test est booleenne//
+let test = 3>5;
+console.log(typeof(test))
+
+const identity = "Bonjour" + " " + "Maxime" + " " + "vous avez" + " " + "15" + " " + "ans"
+let majuscule = identity.toUpperCase();
+// bonjour +  + prenom +  + vous avez +  + 15 +  + ans  //
+console.log(identity)
+console.log(majuscule)
+// majuscule pour mettre identity en maj//
+
