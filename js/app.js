d7f09adb00ee0427ac62f7701f84e7834d243e24
NotMaxivelow01 <mfont@endenschool.fr>
2022-09-26 14:58:50 +0200

    PROMPT

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ console.log("Hello");
 let age;
 age = 15;
 age = prompt("tappez votre age");
-if(age <= 0){
+if(age = 0){
     console.log("vous êtes un poisson panné");
+}
 
-}else if(age <= 120){
+if(age <= 120){
     console.log("Votre âge est validé");
 }else if(age <= 0){
     console.log("Je ne suis pas d'accord");
@@ -38,7 +39,7 @@ let modulo = 5%2;
 console.log(modulo);;
 
 //test est booleenne//
-let test = 3>5;
+let test = 3 > 5;
 console.log(typeof(test));
 
 const identity = "Bonjour" + " " + "Maxime" + " " + "vous avez" + " " + "15" + " " + "ans"
@@ -80,4 +81,29 @@ function positive(num) {
 return num > 0
 }
 positive(5);
-vdh
+
+
+
+
+age = parseInt(age)
+let verifier_nombre = isNaN(age)
+console.log(age)
+console.log(verifier_nombre)
+
+function isValid(userInput){
+    let input = parseInt(userInput);
+    if (isNaN(input)) {
+        console.log("faux");
+        return input;
+}else if (userInput>12 && userInput<90) {
+    console.log("vrais");
+    return input;
+}
+}
+isValid("152");
+
+let resultat = isValid(age);
+    console.log(resultat)
+let resultatinverse = !resultat;
+    console.log(resultatinverse);
+
